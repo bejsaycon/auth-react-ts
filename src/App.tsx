5fe364fec6c331b0d0835454c6ed508ee4aebf92
Bryan Jeff Saycon <bryanjeff.saycon@g.msuiit.edu.ph>
2022-04-18 21:22:03 +0800

    login component initialized

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
-import React from 'react';
-import { Header } from './components/Header';
-import { Home } from './components/Home';
 import { Register } from './components/Register';
 
 function App() {
   return (
     <div className="App">
       <Register />
-      {/* <Header />
-      <Home /> */}
+      
     </div>
   );
 }
